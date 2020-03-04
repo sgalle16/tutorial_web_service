7b6c2187ead4682ae72c304bbbcc105e54e67140
Santiago Gallego Quintero <sgalle16@eafit.edu.co>
2020-03-04 17:13:00 -0500

    Web services con CRUD

---
+++ hygrometer/hygrometer_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HygrometerAppConfig(AppConfig):
+    name = 'hygrometer_app'
---
+++ hygrometer/hygrometer_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Hygrometer
+from .serializers import HygrometerSerializer
+
+class HygrometerViewSet(viewsets.ModelViewSet):
+    queryset = Hygrometer.objects.all().order_by('-created')
+    serializer_class = HygrometerSerializer
